1882ae1691038fb22b066220f9247726a39b4868
Alagbada123 <114651605+Alagbada123@users.noreply.github.com>
2025-02-26 23:17:44 +0100

    Update 2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -5,7 +5,7 @@ import requests
 
 def recurse(subreddit, hot_list=None, after=None):
     """
-    Recursively queries the Reddit API and returns a, 
+    Recursively queries the Reddit API and returns a,
     list containing the titles of all hot articles.
 
     Args:
