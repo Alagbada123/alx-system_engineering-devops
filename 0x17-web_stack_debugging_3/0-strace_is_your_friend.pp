392979181e70fc9ff8f62c6bb43a23ca7f0ac5e6
Abiodun <abiodunalagbada@gmail.com>
2025-02-27 01:13:56 +0100

    find out why Apache is returning a 500 error

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,6 @@
-# automated puppet fix (to find out why Apache is returning a 500 error)
+# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
 
-exec { 'fix_wp_settings': 
-  command => '/bin/sed -i "s/.phpp/.php/g" /var/www/html/wp-settings.php',
-  onlyif => 'grep ".phpp" /var/www/html/wp-settings.php',
-  path => ['/bin', '/usr/bin'],
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
