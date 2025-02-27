610a868f664df8ae7940fe2886ac0b482216586b
Abiodun <abiodunalagbada@gmail.com>
2025-02-27 01:11:49 +0100

    Using strace, find out why Apache is returning a 500 error

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,7 @@
 # automated puppet fix (to find out why Apache is returning a 500 error)
 
-exec { 'Fix wordpress site':
-  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
-  provider => shell,
+exec { 'fix_wp_settings': 
+  command => '/bin/sed -i "s/.phpp/.php/g" /var/www/html/wp-settings.php',
+  onlyif => 'grep ".phpp" /var/www/html/wp-settings.php',
+  path => ['/bin', '/usr/bin'],
 }
